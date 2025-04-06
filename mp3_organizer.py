d5631c0e91172dd7953ceb15c6d320eaeb94ad14
Euclid Agustin <euclidjohnagustin@gmail.com>
2025-04-06 08:26:49 +0800

    Rename script to mp3_organizer.py

--- mp3_organizer.py
+++ mp3_organizer.py
@@ -5,7 +5,7 @@ import mutagen
 from mutagen.mp3 import MP3 
 
 song_dir = 'C:\\Users\\Euclid\\Desktop\\songs - onthespot\\temp' # your directory path
-destination = 'C:\\Users\\Euclid\\Desktop\\songs - onthespot\\gorillaz radio' # desired directory to move files (CHANGE WHEN NEEDED)
+destination = 'C:\\Users\\Euclid\\Desktop\\songs - onthespot\\Arcane - OST' # desired directory to move files (CHANGE WHEN NEEDED)
 
 
 #AUDIO LENGTH CALCULATOR
